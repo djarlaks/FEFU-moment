a033b72548a6a41f84cd4d826a4a1a46eefc26a6
acceptTheVoid <arcad2004@gmail.com>
2022-06-23 11:48:32 +1000

    aboba2

--- 2_term/Informatics/Yashnev_Arkadiy/100.cpp
+++ 2_term/Informatics/Yashnev_Arkadiy/100.cpp
@@ -12,30 +12,31 @@ void prefix(int *pi, string const &string) {
         pi[i] = j;
     }
 }
- 
+
 int KMP(const string& str, const string &sample) {
+    int ans = -1;
     string line = sample + '&' + str;
     auto l_size = line.size();
     auto s_size = sample.size();
     int *pi = new int[l_size]{};
     prefix(pi, line);
-    for (size_t i = s_size+1; i < l_size; ++i) {
+    for (size_t i = s_size + 1; i < l_size; ++i) {
         if (pi[i] == s_size) {
-            delete[] pi;
-            return i - 2 * s_size;
-            cout << i-2*s_size << ' ';
+            ans = i - 2 * s_size;
+            break;
         }
     }
+
     delete[] pi;
-    return -1;
+    return ans;
 }
 
 int main() {
     string str, comp;
     cin >> str;
     cin >> comp;
-    comp = comp + comp;
-    size_t result = KMP(comp, str);
+    comp += comp;
+    int result = KMP(comp, str);
     cout << result;
     return 0;
 }
